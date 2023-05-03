324c8eb441c3510627270be5492c424086c7b5ba
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-05-03 11:44:55 +0200

    bugfix/task 01 - button.disabled added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const buttonStart = document.querySelector('[data-start]');
 const buttonStop = document.querySelector('[data-stop]');
 const bodyBackground = document.querySelector('body');
+let colorChange = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -11,8 +12,10 @@ buttonStart.addEventListener('click', () => {
     const newColor = getRandomHexColor();
     bodyBackground.style.backgroundColor = newColor;
   }, 1000);
+  buttonStart.disabled = true;
 });
 
 buttonStop.addEventListener('click', () => {
   clearInterval(colorChange);
+  buttonStart.disabled = false;
 });
