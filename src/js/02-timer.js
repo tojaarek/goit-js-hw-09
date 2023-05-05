9c39a86835d09896c3cab79ac3f84ee52471fd86
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-05-05 15:22:43 +0200

    task 02 update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,15 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+let interval = null;
+const daySpan = document.querySelector('[data-days]');
+const hourSpan = document.querySelector('[data-hours]');
+const minuteSpan = document.querySelector('[data-minutes]');
+const secondSpan = document.querySelector('[data-seconds]');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const buttonReset = document.querySelector('[data-reset]');
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -16,22 +25,29 @@ const options = {
     } else {
       buttonStart.disabled = false;
       buttonStart.addEventListener('click', () => {
+        buttonStart.disabled = true;
+        buttonStop.disabled = false;
         interval = setInterval(() => {
-          const ms = date.getTime() - new Date().getTime();
-          console.log(ms);
-          function convertMs(ms) {
-            const second = 1000;
-            const minute = second * 60;
-            const hour = minute * 60;
-            const day = hour * 24;
-
-            const days = Math.floor(ms / day);
-            const hours = Math.floor((ms % day) / hour);
-            const minutes = Math.floor(((ms % day) % hour) / minute);
-            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-            return { days, hours, minutes, seconds };
+          let ms = date.getTime() - new Date().getTime();
+          if (ms < 0) {
+            clearInterval(interval);
+            return;
           }
+          console.log(ms);
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
+
+          const days = Math.floor(ms / day);
+          const hours = Math.floor((ms % day) / hour);
+          const minutes = Math.floor(((ms % day) % hour) / minute);
+          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+          daySpan.innerHTML = days.toString().padStart(2, '0');
+          hourSpan.innerHTML = hours.toString().padStart(2, '0');
+          minuteSpan.innerHTML = minutes.toString().padStart(2, '0');
+          secondSpan.innerHTML = seconds.toString().padStart(2, '0');
         }, 1000);
       });
     }
@@ -40,9 +56,17 @@ const options = {
 
 const picker = document.querySelector('#datetime-picker');
 const flatpicker = flatpickr(picker, options);
-const daySpan = document.querySelector('[data-days]');
-const hourSpan = document.querySelector('[data-hours]');
-const minuteSpan = document.querySelector('[data-minutes]');
-const secondSpan = document.querySelector('[data-seconds]');
-const buttonStart = document.querySelector('[data-start]');
-let interval;
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(interval);
+  buttonReset.disabled = false;
+  buttonStop.disabled = true;
+});
+
+buttonReset.addEventListener('click', () => {
+  buttonReset.disabled = true;
+  daySpan.innerHTML = '00';
+  hourSpan.innerHTML = '00';
+  minuteSpan.innerHTML = '00';
+  secondSpan.innerHTML = '00';
+});
