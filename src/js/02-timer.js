b421ca526f0870c76b27ce0a0cc2add87d30e2dc
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-05-05 16:05:58 +0200

    task 02 update / add styles

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 let interval = null;
+let isCounting = false;
 const daySpan = document.querySelector('[data-days]');
 const hourSpan = document.querySelector('[data-hours]');
 const minuteSpan = document.querySelector('[data-minutes]');
@@ -24,32 +25,36 @@ const options = {
       Notiflix.Notify.failure('Please choose a date in the future');
     } else {
       buttonStart.disabled = false;
-      buttonStart.addEventListener('click', () => {
-        buttonStart.disabled = true;
-        buttonStop.disabled = false;
-        interval = setInterval(() => {
-          let ms = date.getTime() - new Date().getTime();
-          if (ms < 0) {
-            clearInterval(interval);
-            return;
-          }
-          console.log(ms);
-          const second = 1000;
-          const minute = second * 60;
-          const hour = minute * 60;
-          const day = hour * 24;
+      if (!isCounting) {
+        isCounting = true;
+        buttonStart.disabled = false;
+        buttonStart.addEventListener('click', () => {
+          buttonStart.disabled = true;
+          buttonStop.disabled = false;
+          interval = setInterval(() => {
+            let ms = date.getTime() - new Date().getTime();
+            if (ms < 0) {
+              clearInterval(interval);
+              return;
+            }
+            console.log(ms);
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
 
-          const days = Math.floor(ms / day);
-          const hours = Math.floor((ms % day) / hour);
-          const minutes = Math.floor(((ms % day) % hour) / minute);
-          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+            const days = Math.floor(ms / day);
+            const hours = Math.floor((ms % day) / hour);
+            const minutes = Math.floor(((ms % day) % hour) / minute);
+            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-          daySpan.innerHTML = days.toString().padStart(2, '0');
-          hourSpan.innerHTML = hours.toString().padStart(2, '0');
-          minuteSpan.innerHTML = minutes.toString().padStart(2, '0');
-          secondSpan.innerHTML = seconds.toString().padStart(2, '0');
-        }, 1000);
-      });
+            daySpan.innerHTML = days.toString().padStart(2, '0');
+            hourSpan.innerHTML = hours.toString().padStart(2, '0');
+            minuteSpan.innerHTML = minutes.toString().padStart(2, '0');
+            secondSpan.innerHTML = seconds.toString().padStart(2, '0');
+          }, 1000);
+        });
+      }
     }
   },
 };
@@ -64,6 +69,7 @@ buttonStop.addEventListener('click', () => {
 });
 
 buttonReset.addEventListener('click', () => {
+  Notiflix.Notify.info('Choose the date once again to restart the counter');
   buttonReset.disabled = true;
   daySpan.innerHTML = '00';
   hourSpan.innerHTML = '00';
